a7313da8c75dd9cb48f18b7ccba9492babac4829
perumal <ssirangjivi@gmail.com>
2025-10-01 22:02:46 +0530

    Time: 3 ms (61.53%), Space: 13.5 MB (30.54%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        n=len(nums)
+        missing =n
+        for i in range(n):
+            missing ^=i ^ nums[i]
+        return missing
+            
